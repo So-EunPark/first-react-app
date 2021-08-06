32326be411f3f2824353f36a8b743fb525f77049
soeun <70000city@gmail.com>
2021-08-06 18:16:29 +0900

    react component의 lifeCycle을 공부해보았다!

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import MyName from './MyName';
-
+import Counter from './Counter';
 function App() {
   return (
     <MyName 
       name='리액트으'
-    />
+    />,
+    <Counter />
   );
 }
 
